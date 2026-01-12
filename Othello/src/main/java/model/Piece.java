68a55109361f6b6b584c86f18649e57a90aa55a8
CarolainQuesada <carolain211222@gmail.com>
2026-01-12 01:54:10 -0600

    Add Node class with eight directional links

--- Othello/src/main/java/model/Piece.java
+++ Othello/src/main/java/model/Piece.java
@@ -1,20 +1,27 @@
-
 package model;
 
 /**
+ * Represents a game piece in the board.
+ * A piece has a color that identifies the player.
  *
- * @author Usuario
+ * @author Carolain Quesada
  */
 public class Piece {
 
     private char color;
 
+    /**
+     *
+     * @param color Character representing the piece color
+     */
     public Piece(char color) {
         this.color = color;
     }
 
+    /**
+     * @return piece color
+     */
     public char getColor() {
         return color;
     }
 }
-
