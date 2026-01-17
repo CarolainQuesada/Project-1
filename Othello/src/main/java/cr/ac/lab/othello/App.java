fe4fc5a733900c4d66cf0a0d7af396304edcd22b
MathewRamirez <mathewramirez65@gmail.com>
2026-01-16 23:06:15 -0600

    comprobate use
    For test

--- Othello/src/main/java/cr/ac/lab/othello/App.java
+++ Othello/src/main/java/cr/ac/lab/othello/App.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Board;
+import model.Node;
+import model.Piece; // solo si ya existe
 
 import java.io.IOException;
 
@@ -32,7 +35,36 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
-    }
+   
 
+
+
+
+        // Crear un tablero 5x5
+        Board board = new Board(5, 5);
+
+        // Imprimir tablero vacío
+        System.out.println("Tablero vacío:");
+        board.printBoard();
+
+        // Colocar piezas manualmente para probar
+        Node start = board.getStart();
+
+        // Colocar una pieza en (0,0)
+        start.setPiece(new Piece('c'));
+
+        // Moverse a (0,2)
+        Node node02 = board.getNode(0, 2);
+        node02.setPiece(new Piece('c'));
+
+        // Moverse a (2,3)
+       Node node23 = board.getNode(2, 3); 
+ 
+        // 2. Le pones la pieza al nodo guardado
+       node23.setPiece(new Piece('V'));
+
+        // Imprimir tablero con piezas
+        System.out.println("\nTablero con piezas:");
+        board.printBoard();
+    }
 }
