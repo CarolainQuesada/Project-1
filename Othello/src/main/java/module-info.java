ad9e60eb12500861b278b4e03ad03ac44d8dead4
CarolainQuesada <carolain211222@gmail.com>
2026-01-19 21:46:41 -0600

    Configure module exports for Othello packages

--- Othello/src/main/java/module-info.java
+++ Othello/src/main/java/module-info.java
@@ -1,7 +1,13 @@
+/**
+ * Main module of the Othello game.
+ */
 module cr.ac.lab.othello {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens cr.ac.lab.othello to javafx.fxml;
     exports cr.ac.lab.othello;
+    exports cr.ac.lab.othello.controller;
+    exports cr.ac.lab.othello.model;
+    exports cr.ac.lab.othello.view;
 }
